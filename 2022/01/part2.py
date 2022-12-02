1987123c84350ac22146dc55a5f69c16a652c3b8
Gischenato <gischenato@gmail.com>
2022-12-02 02:28:54 -0300

    menos linhas

--- 2022/01/part2.py
+++ 2022/01/part2.py
@@ -2,12 +2,8 @@ biggest = []
 acc = 0
 for line in open('in'):
     if line.strip() == '':
-        if len(biggest) == 3:
-            biggest = sorted(biggest)
-            biggest[0] = max(acc, biggest[0])
-        else:
-            biggest.append(acc)
+        biggest.append(acc)
         acc = 0
         continue
     acc += int(line)
-print(sum(biggest))
+print(sum((sorted(biggest, reverse=True)[:3])))
