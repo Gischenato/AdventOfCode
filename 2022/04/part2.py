c9713dac262320bb8f58dbf94255c89738166ddc
Giovanni Schenato <45777166+Gischenato@users.noreply.github.com>
2022-12-04 18:27:25 -0300

    Update part2.py

--- 2022/04/part2.py
+++ 2022/04/part2.py
@@ -5,6 +5,6 @@ for line in open('in'):
     elf1, elf2 = [[int(v1), int(v2)] for v1,v2 in (x.split('-') for x in line.strip().split(','))]
     set1.update(i for i in range(elf1[0], elf1[1]+1))
     set2.update(i for i in range(elf2[0], elf2[1]+1))
-    if len(set1 & set2) != 0 or len(set1 & set2) != 0:
+    if len(set1 & set2) != 0:
         total+=1
 print(total)
