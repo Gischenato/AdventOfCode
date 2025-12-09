1b259b00a2479e0c2e262662da2961e57dd2b870
Gischenato <gischenato@gmail.com>
2025-12-09 11:56:30 -0300

    2025-03

--- 2025/03/part1.py
+++ 2025/03/part1.py
@@ -19,19 +19,3 @@ for line in open('in'):
     print(biggest_number)
 
 print(tot)
------
-tot = 0
-for line in open('in'):
-    line = list(line.strip())
-    curr = 1
-
-    while True:
-        if len(line) == 12 or curr >= len(line): break
-        if int(line[curr]) > int(line[curr-1]):
-            line.pop(curr-1)
-            curr -= 1
-        else: curr += 1
-        if curr == 0: curr = 1
-    tot += int(''.join(line[:12]))
-
-print(tot)
